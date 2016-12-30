035274e31709b2a7622abb2180b2697eb66a2936
柳剑 <liujian@kunteng.org>
2016-12-30 16:00:39 +0800

    简单的springboot Security配置

---
+++ Chapter02/src/main/java/com/changgx/Controller.java
@@ -0,0 +1,31 @@
+package com.changgx;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Administrator 2016/12/30
+ */
+@RestController
+public class Controller {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "hello";
+    }
+}
---
+++ Chapter02/src/main/java/com/changgx/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.changgx;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 自定义Security配置
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/home","/hello").permitAll() //需要保护的url
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()       //被拦截后跳转的的登陆页路径
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("123456").roles("USER");
+    }
+}
